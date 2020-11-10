a4e3dec267cb23a9cbf6c1259a6ef4ce2ca7558d
Mgen <m@mgenware.com>
2020-11-11 00:07:14 +0800

    Update project

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,13 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   endOfLine: 'lf',
+  printWidth: 100,
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        parser: 'html',
+      },
+    },
+  ],
 };
--- tests/module.test.ts
+++ tests/module.test.ts
@@ -1,10 +1,6 @@
-import { promisify } from 'util';
-import { stat } from 'fs';
-const statAsync = promisify(stat);
+import { promises as fsPromises } from 'fs';
 import * as assert from 'assert';
 
-const expect = assert.equal;
-
 it('Verify type definition files', async () => {
-  expect((await statAsync('./dist/main.d.ts')).isFile(), true);
+  assert.ok((await fsPromises.stat('./dist/main.d.ts')).isFile());
 });
